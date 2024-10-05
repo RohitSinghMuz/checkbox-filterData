b85fe5e69852b8f750c0dc68ea085f3b94d21892
Makireddy Salman Raju <makireddy.raju@extwebtech.in>
2024-10-06 02:25:47 +0530

    filter map checkbox

--- App.tsx
+++ App.tsx
@@ -1,118 +1,132 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const arrayData = [
+    { id: 1, name: "Delhi" },
+    { id: 2, name: "Kolkata" },
+    { id: 3, name: "Mohali" },
+    { id: 4, name: "Hyderabad" },
+    { id: 5, name: "Pune" },
+    { id: 6, name: "Chennai" },
+    { id: 7, name: "Bangalore" },
+    { id: 8, name: "Patna" },
+  ];
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const imgData = [
+    "1",
+    "2",
+    "Mohali",
+    "Hyderabad",
+    "Pune",
+    "Chennai",
+    "Bangalore",
+    "Patna",
+  ];
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const [sName, setSName] = useState("");
+  const [sData, setsData] = useState([]);
+  const [currIndex, setcurrIndex] = useState(0);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+  const filterData = arrayData.filter((item, index) =>
+    item.name.toLowerCase().includes(sName.toLowerCase())
   );
-}
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const handleSelect = (idx) => {
+    const selectData = arrayData.find((item, index) => item?.id == idx);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    const isSelected = sData.some((item, index) => item?.id == selectData?.id);
+    if (isSelected) {
+      const unSelected = sData.filter((item, index) => item.id !== idx);
+      setsData(unSelected);
+    } else {
+      setsData([...sData, selectData]);
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  const handleSelectAll = () => {
+    setsData([...filterData]);
+  };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  const handleUnselectAll = () => {
+    setsData([]);
+  };
+
+  const imgLength = imgData.length;
+
+  const handleSelectLeft = () => {
+    const leftData = (currIndex - 1 + imgLength) % imgLength;
+    setcurrIndex(leftData);
+  };
+
+  const handleSelectRight = () => {
+    const leftData = (currIndex + 1) % imgLength;
+    setcurrIndex(leftData);
+  };
+
+  useEffect(() => {
+    const AutoMove = setInterval(() => {
+      const leftData = (currIndex + 1) % imgLength;
+      setcurrIndex(leftData);
+    }, 3000);
+
+    return () => clearInterval(AutoMove);
+  }, [currIndex, imgLength]);
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Enter Name"
+        style={{ margin: 2, padding: 10, borderWidth: 1, borderColor: "grey" }}
+        onChangeText={(text) => setSName(text)}
+      />
+
+      {filterData?.length > 0 ? (
+        filterData.map((item, index) => {
+          return (
+            <View>
+              <TouchableOpacity onPress={() => handleSelect(item.id)}>
+                <Text
+                  style={{
+                    padding: 10,
+                    backgroundColor: sData.some(
+                      (ele, index) => ele?.id === item?.id
+                    )
+                      ? "red"
+                      : "white",
+                  }}
+                >
+                  {item.name}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          );
+        })
+      ) : (
+        <Text style={{ padding: 10 }}>name not found</Text>
+      )}
+
+      <TouchableOpacity onPress={handleSelectAll}>
+        <Text style={{ padding: 10 }}>Select All</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleUnselectAll}>
+        <Text style={{ padding: 10 }}>Unselect All</Text>
+      </TouchableOpacity>
+
+      <Text style={{ padding: 10 }}>{imgData[currIndex]}</Text>
+
+      <TouchableOpacity onPress={handleSelectLeft}>
+        <Text style={{ padding: 10 }}>Left</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleSelectRight}>
+        <Text style={{ padding: 10 }}>Right</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default App;
+
+// carousel and filter with map and select checkobx
